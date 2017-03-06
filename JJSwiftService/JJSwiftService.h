3bcf29712011bdf6f7f4e49655a2c4e46dc65f0e
JJ <gongjian_001@126.com>
2017-03-06 09:54:20 +0800

    Initial Commit

---
+++ JJSwiftService/JJSwiftService.h
@@ -0,0 +1,19 @@
+//
+//  JJSwiftService.h
+//  JJSwiftService
+//
+//  Created by JJ on 3/6/17.
+//  Copyright © 2017 jianjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJSwiftService.
+FOUNDATION_EXPORT double JJSwiftServiceVersionNumber;
+
+//! Project version string for JJSwiftService.
+FOUNDATION_EXPORT const unsigned char JJSwiftServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJSwiftService/PublicHeader.h>
+
+
